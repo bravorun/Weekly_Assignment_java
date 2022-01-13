421aa309365d8b345df702340809617a4f1e516c
Ranjan Kumar Chaurasia <VE00YM346@YMI.COM>
2022-01-13 22:28:07 +0530

    Update

--- src/com/day_2/Q3/Account.java
+++ src/com/day_2/Q3/Account.java
@@ -1,5 +1,19 @@
 package com.day_2.Q3;
 
+import java.util.Random;
+
 public class Account {
+private String memberName;
+private String accountNumber;
+private Double accountBalance;
+public Account(String memberName, Double accountBalance) {
+	Random rand=new Random();
+	accountNumber=10000+rand.nextInt(89999)+"";
+	this.memberName = memberName;
+	this.accountBalance = accountBalance;
+}
+public void deposit(double amt) {
+	accountBalance+=amt;
+}
 
 }
