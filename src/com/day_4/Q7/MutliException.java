697999180480db87334af96292a866dc17a5cb34
Ranjan Kumar Chaurasia <VE00YM346@YMI.COM>
2022-01-20 22:25:43 +0530

    Exception Handiling Final

--- src/com/day_4/Q7/MutliException.java
+++ src/com/day_4/Q7/MutliException.java
@@ -4,12 +4,22 @@ import java.io.IOException;
 import java.util.InputMismatchException;
 
 public class MutliException {
-	void throwException()  {
-		throw new InputMismatchException();
-		throw new IOException();
+	void throwException() throws InputMismatchException,IOException{
+		System.out.println("In exception Block");
 		
 	}
 	public static void main(String args[]) {
-		throwException();
+		MutliException exp=new MutliException();
+		try {
+			exp.throwException();
+		} catch (InputMismatchException | IOException e) {
+			e.printStackTrace();
+		}
+		/*
+		 * Single throw catch block has been added because InputMisMatchException and IOException
+		 *  because from java 7 we can have multiple Exception in single catch and by default
+		 *  Exception parameter if Final
+		 */
+		
 	}
 }
